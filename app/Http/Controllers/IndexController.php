08d5b7a0434dd5c678a7a1aabe310bbbee9298d2
thelast7 <takimanwar@gmail.com>
2018-08-15 09:49:11 +0700

    undo

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,13 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Product;
 
 class IndexController extends Controller
 {
-    public function index()
-    {
-        $products = Product::where('status', 1);
-        return view('layouts._home.index', compact('products'));
-    }
+    //
 }
