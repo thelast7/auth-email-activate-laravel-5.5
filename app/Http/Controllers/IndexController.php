4c52cf5a578557660574eefa6d1948f70b10e926
thelast7 <takimanwar@gmail.com>
2018-08-20 16:03:54 +0700

    controller index

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
+use App\Category;
 
 class IndexController extends Controller
 {
     public function index()
     {
-        return view('layouts._home.index');
+        $products = Product::where('status', 1)->limit(4)->get();
+        return view('layouts._home.index', compact('products'));
     }
 }
